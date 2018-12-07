436cba7b438f0197ea5096f47c542edf8c6f3d7b
Olciandro <olciandro.magalhaes@basis.com.br>
2018-12-07 14:05:32 -0200

    Teste unitario com a entidade Estado ok.

--- quero-alugar/src/main/java/br/com/queroalugar/service/EstadoService.java
+++ quero-alugar/src/main/java/br/com/queroalugar/service/EstadoService.java
@@ -2,12 +2,13 @@ package br.com.queroalugar.service;
 
 import br.com.queroalugar.domain.Estado;
 import br.com.queroalugar.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EstadoService{
 
-    EstadoRepository repository;
+    private EstadoRepository repository;
 
     public void salvar(Estado estado){
 
@@ -15,5 +16,14 @@ public class EstadoService{
             System.out.println("Objeto nulo!");
         }
         repository.save(estado);
+    }
+
+    public EstadoRepository getRepository() {
+        return repository;
+    }
+
+    @Autowired
+    public void setRepository(EstadoRepository repository) {
+        this.repository = repository;
     }
 }
--- quero-alugar/src/test/java/br/com/queroalugar/EstadoTest.java
+++ quero-alugar/src/test/java/br/com/queroalugar/EstadoTest.java
@@ -3,8 +3,13 @@ package br.com.queroalugar;
 import br.com.queroalugar.domain.Estado;
 import br.com.queroalugar.service.EstadoService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class EstadoTest {
 
     @Autowired
